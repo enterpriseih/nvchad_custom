0105ea4ecd0fc0fddd0a7233c098e4624a26fe08
yanghongwei <yhw1813@126.com>
2023-01-15 11:46:04 +0800

    add jdtls and add lspconfig mapping to jdtls

--- init.lua
+++ init.lua
@@ -5,3 +5,11 @@ local autocmd = vim.api.nvim_create_autocmd
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+vim.api.nvim_create_autocmd("Filetype", {
+	pattern = "java", -- autocmd to start jdtls
+	callback = function()
+		print("java file autocmd")
+		local config = require("custom.plugins.jdtls")
+		if config.root_dir and config.root_dir ~= "" then require("jdtls").start_or_attach(config) end
+  end,
+})
--- plugins/init.lua
+++ plugins/init.lua
@@ -1,5 +1,14 @@
 local overrides = require "custom.plugins.overrides"
 
 return {
-
+  ["folke/which-key.nvim"] = {
+    disable = false,
+  },
+  ["neovim/nvim-lspconfig"] = {
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+  },
+  ["mfussenegger/nvim-jdtls"] = {},
 }
